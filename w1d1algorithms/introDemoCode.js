29a24b45295d162b83c155144c8f952a2529df42
Eyasu Gebrehiwot <jgebrehiwot@gmail.com>
2021-02-22 20:39:44 -0600

    This is the first assignment commited to git hub for intoJs class for Prof Levi

---
+++ w1d1algorithms/introDemoCode.js
@@ -0,0 +1,10 @@
+'use strict'
+let x = 5;
+let y = 6; 
+console.log(x + y);
+
+x = 10;
+console.log(x + y);
+const ourPlantName = 'Earth';
+let currentUserName = "Josh";
+console.log('Our beautiful planet is called. ' + ourPlantName+' One of the leaders of big clans on it is '+currentUserName);
